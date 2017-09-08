45ea895783703f4246cfd20e4c3b3410b88b6ff4
Jingky Sulpico <jingkysulpico05@gmail.com>
2017-09-08 11:39:41 +0800

    great!

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,8 @@ public class HelloWorld {
 	public static void main(String[] args){
 		for(int x=1; x<=10; x++){
 			System.out.println("Hello world ..."+x);
+			
 		}
+		System.out.println("Great!");
 	}
 }
